cd7f6d5a38ee90836c0f791904e21ad43b9e0714
NPalia <alberto.nicolas.palia@gmail.com>
2022-06-01 21:51:38 -0300

    Homework JSII

---
+++ Practica/scrip.js
@@ -0,0 +1,11 @@
+function suma(x, y) {
+    // "x" e "y" son números
+    // Suma "x" e "y" juntos y devuelve el valor
+    // Tu código:
+    var x = Number
+    var y = Number
+    var suma = x + y 
+    return suma
+    }
+
+suma(10,1)
